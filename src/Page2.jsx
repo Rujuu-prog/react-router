74db275a9e0e09b028c042f7834b113b399a86aa
rujuu <81368541+Rujuu-prog@users.noreply.github.com>
2021-04-11 17:10:11 +0900

    add url router

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 export const Page2 = () => {
   return (
     <div>
       <h1>Page2のページです</h1>
+      <Link to="/page2/999">URL Parwmeter</Link>
+      <br />
+      <Link to="/page2/999?name=hogehoge">Query Parwmeter</Link>
     </div>
   );
 };
